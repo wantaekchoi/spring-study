ab95c0e898d1eee2e3911ac7304efca7be2fa016
wantaek <wantaek@kakao.com>
2022-07-02 18:02:54 +0900

    add SingletonService and test code private creator => prevent new keywords

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import javax.lang.model.SourceVersion;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -21,11 +23,27 @@ public class SingletonTest {
         // 2. 조회: 호출할 때 마다 객체를 생성
         MemberService memberService2 = ac.memberService();
 
-        // 3. 참조 값이 다른 것을 확인
+        // 3. 참조: 값이 다른 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
         // 4. memberService1 != memberService2 => 호출할 때 마다 객체를 생성하면 자원 낭비가 심하다
-        Assertions.assertThat(memberService1).isNotEqualTo(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+        // 1. 조회: 싱글톤 객체 호출
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // 2. 참조: 값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // 3. singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
